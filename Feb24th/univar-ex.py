637bcb619bae46b4cf559fd0ae1a0e5aa95e51c5
wbalmer <williamobalmer@gmail.com>
2020-02-22 19:02:56 -0500

    adds units to figures

--- Feb24th/univar-ex.py
+++ Feb24th/univar-ex.py
@@ -1,4 +1,5 @@
 # imports
+import re # for string comp.
 import numpy as np # for computation and arrays
 import pandas as pd # for datatables
 import matplotlib.pyplot as plt # for plotting
@@ -11,6 +12,21 @@ from scipy.stats import shapiro
 
 # read in data
 data = pd.read_csv('TESS_planets.csv', skiprows=78)
+physicals = ['pl_orbper', 'pl_orbsmax', 'pl_orbeccen', 'pl_orbincl', 'pl_bmassj', 'pl_radj', 'pl_dens', 'st_dist', 'st_optmag', 'st_teff', 'st_mass', 'st_rad']
+col_unit_dict = {
+    "pl_pnum":"Number of Planets in System",
+    "pl_orbper":"Orbital Period [days]",
+    "pl_orbsmax":"Orbit Semi-Major Axis [au]",
+    "pl_orbeccen":"Eccentricity",
+    "pl_orbincl":"Inclination [deg]",
+    "pl_bmassj":"Planet Mass [Jupiter mass]",
+    "pl_radj":"Planet Radius [Jupiter radii]",
+    "pl_dens":"Planet Density [g/cm**3]",
+    "st_dist":"Distance [pc]",
+    "st_optmag":"Optical Magnitude [mag]",
+    "st_teff":"Effective Temperature [K]",
+    "st_mass":"Stellar Mass [Solar mass]",
+    "st_rad":"Stellar Radius [Solar radii]"}
 
 # ask which column the user wants to plot
 col = input('Pick a column. For options enter a question mark (?) : ')
@@ -18,7 +34,10 @@ col = str(col)
 if col =='?':
     print(data.columns)
     col = input('Pick a column from the above list: ')
+elif col not in physicals:
+    print('WARNING: That column is not physically meaninful, you will likely encounter an error.')
 
+print('Creating a KDE for '+col_unit_dict[col]+'...')
 # ask which kernel to use
 kern = str(input('Pick your kernel. Options are: gau, cos, tri, uni, biw, epa, triw. In doubt? try gau) : '))
 
@@ -37,15 +56,17 @@ def plotthefig(save='n', g='n'):
     x = np.linspace(np.min(kde.support), np.max(kde.support), len(d)) # creates xvalues, range for plotting
     mu, std = norm.fit(d) # fits single peak gaussian to data
 
-    weights = np.ones_like(d)/float(len(d))
-    p = norm.pdf(x,mu,std)
-    W, pW = shapiro(d)
-    gaussianity = 'Data \n(Shapiro W='+str(round(W, 4))+')'
+    weights = np.ones_like(d)/float(len(d)) # normalize bins
+    p = norm.pdf(x,mu,std) # create normal fit
+    W, pW = shapiro(d) # normality test on data
+    gaussianity = 'Data \n(Shapiro W='+str(round(W, 4))+')' # string to label data w/normality fit
+    namestr = col_unit_dict[col] # string for titles and labels
+
     # set plotting stuff
     f, ax = plt.subplots(figsize=(7,5),dpi=100)
-    plt.xlabel(d.name)
+    plt.xlabel(namestr)
     plt.ylabel('Occurrence (data) \nNormalized Likelihood (models)')
-    plt.title('Kernel Density Estimation of column ' +d.name)
+    plt.title('Kernel Density Estimation of column ' +re.sub('[[@*&?].*[]@*&?]','',namestr))
 
     ax.hist(d, bins=15, weights=weights, label=gaussianity, alpha=0.8, zorder=1, color='xkcd:mauve')
     sb.scatterplot(kde.support, kde.density/np.max(kde.density), zorder=2, color='xkcd:light pink', linewidth=0, s=20, label='KDE')
@@ -78,7 +99,6 @@ else:
     pass
 
 # to quickly generate fits for all physical parameters, uncomment and run only this loop
-#physicals = ['pl_orbper', 'pl_orbsmax', 'pl_orbeccen', 'pl_orbincl', 'pl_bmassj', 'pl_radj', 'pl_dens', 'st_dist', 'gaia_dist', 'st_optmag', 'st_teff', 'st_mass', 'st_rad']
 #for col in physicals:
 #    d = data[col].dropna()
 #    kde = sm.nonparametric.KDEUnivariate(d)
